fbea33d41857214f1e5221f3ba033df86f0f4ea4
Vipul Nataraj <vnataraj@purdue.edu>
2013-09-21 16:51:09 -0400

    removed debugging output

--- server/getTweets.php
+++ server/getTweets.php
@@ -7,7 +7,6 @@ $query='SELECT * FROM `cached_tweets` WHERE `text` LIKE CONVERT(_utf8 \'%#art%\'
 $result=$mysqli->query($query);
 
 if($result){
-	echo "\n yay we're here";
 	while($row=$result->fetch_assoc()){	
 		printf("%s,%s,%s,%s,%s,%s,%s,%s\n",$row['id'], $row['createdAt'], $row['retweetCount'], $row['latitude'], $row['longitude'], $row['user_id'], $row['user_name'], $row['text']);
 	}
