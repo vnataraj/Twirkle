2948d224e7d4f59228fac3236bb347d2f95d5abe
Vipul Nataraj <vnataraj@purdue.edu>
2013-09-21 09:03:39 -0400

    hopefully add works now

--- server/addTweet.php
+++ server/addTweet.php
@@ -7,14 +7,14 @@
 include 'database.php';
 $id=$_GET["id"];
 $createdAt=$_GET["createdAt"];
-$favoriteCount=$_GET["favoriteCount"];
+$retweetCount=$_GET["retweetCount"];
 $latitude=$_GET["latitude"];
 $longitude=$_GET["longitude"];
 $user_id=$_GET["user_id"];
 $user_name = $_GET["user_name"];
 $text = $_GET["text"];
 
-$result=$mysqli->query("INSERT INTO tweets (id, createdAt, favoriteCount, latitude, longitude, user_id, user_name, text) VALUES ('$id', '$createdAt', '$favoriteCount', '$latitude', '$longitude', '$user_id', '$user_name', '$text')");
+$result=$mysqli->query("INSERT INTO cached_tweets (id, createdAt, retweetCount, latitude, longitude, user_id, user_name, text) VALUES ('$id', '$createdAt', '$retweetCount', '$latitude', '$longitude', '$user_id', '$user_name', '$text')");
 
 if($result==false){
 	echo FAILURE;
